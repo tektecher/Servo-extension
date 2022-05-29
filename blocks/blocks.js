62c10c04d97741ae0acd6197b1f5f5c0cbe00485
Saeed Desouky <saeeddesouky71@gmail.com>
2022-05-29 16:25:40 +0200

    initial commit

---
+++ blocks/blocks.js
@@ -0,0 +1,60 @@
+Blockly.defineBlocksWithJsonArray(
+[{
+  "type": "servo",
+  "message0": "Servo pin %1 set angle %2 °",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "pin",
+      "options": [
+          [
+              "OUT1",
+              "14"
+          ],
+          [
+              "OUT2",
+              "26"
+          ],
+          [
+              "OUT3",
+              "2"
+          ],
+          [
+              "OUT4",
+              "27"
+          ],
+          [
+              "OUT5",
+              "25"
+          ],
+          [
+              "OUT6",
+              "33"
+          ],
+          [
+              "OUT7",
+              "32"
+          ],
+          [
+              "OUT9",
+              "4"
+          ],
+          [
+              "OUT10",
+              "5"
+          ]
+    ]},
+    {
+      "type": "input_value",
+      "name": "angle",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#0271D9",
+  "tooltip": "",
+  "helpUrl": ""
+}]
+);
---
+++ blocks/generators.js
@@ -0,0 +1,9 @@
+Blockly.Python['servo'] = function(block) {
+  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
+  Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
+
+  var value_pin = block.getFieldValue('pin');
+  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
+  var code = `PWM(Pin(${value_pin}), freq=50).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
+  return code;
+};
