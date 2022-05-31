b924154a9fe4a300791926d0bff470ce2610d458
tektecher <78994944+tektecher@users.noreply.github.com>
2022-05-31 17:50:45 +0200

    Update blocks.js

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -55,6 +55,63 @@ Blockly.defineBlocksWithJsonArray(
   "nextStatement": null,
   "colour": "#0271D9",
   "tooltip": "",
-  "helpUrl": ""
+  "helpUrl": "",
+  
+"type": "Bigservo",
+  "message0": "Big Servo pin %1 set angle %2 °",
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "pin",
+      "options": [
+          [
+              "OUT1",
+              "14"
+          ],
+          [
+              "OUT2",
+              "26"
+          ],
+          [
+              "OUT3",
+              "2"
+          ],
+          [
+              "OUT4",
+              "27"
+          ],
+          [
+              "OUT5",
+              "25"
+          ],
+          [
+              "OUT6",
+              "33"
+          ],
+          [
+              "OUT7",
+              "32"
+          ],
+          [
+              "OUT9",
+              "4"
+          ],
+          [
+              "OUT10",
+              "5"
+          ]
+    ]},
+    {
+      "type": "input_value",
+      "name": "angle",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": "#0271D9",
+  "tooltip": "",
+  "helpUrl": ""   
 }]
 );
