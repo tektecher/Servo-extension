f0ef30d585e194ab944931c2db25989f7fae26db
tektecher <78994944+tektecher@users.noreply.github.com>
2022-05-31 17:59:44 +0200

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -9,12 +9,3 @@ Blockly.Python['servo'] = function(block) {
 };
 
 
-Blockly.Python['Bigservo'] = function(block) {
-  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-  Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-
-  var value_pin = block.getFieldValue('pin');
-  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=100).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
-  return code;
-};
