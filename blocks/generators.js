298751e354bbd963a1e35a59b036f66520eb7d02
Saeed Desouky <saeeddesouky71@gmail.com>
2022-05-31 18:03:02 +0200

    support big

--- blocks/generators.js
+++ blocks/generators.js
@@ -7,3 +7,14 @@ Blockly.Python['servo'] = function(block) {
   var code = `PWM(Pin(${value_pin}), freq=50).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
   return code;
 };
+
+
+Blockly.Python['Bigservo'] = function(block) {
+  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
+  Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
+
+  var value_pin = block.getFieldValue('pin');
+  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
+  var code = `PWM(Pin(${value_pin}), freq=100).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
+  return code;
+};
